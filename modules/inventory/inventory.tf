409bbe037df3c7cda229c3e6c70725a9dae101f3
GraceYang <graceyang@graceyangs-mbp.kr.ibm.com>
2020-02-05 17:25:29 +0900

    Modify var syntax

--- modules/inventory/inventory.tf
+++ modules/inventory/inventory.tf
@@ -4,7 +4,7 @@
 data "template_file" "master_block" {
   count = "${var.master_node_count}"
   template = "$${hostname} openshift_ip=$${ip} openshift_node_group_name='node-config-master'"
-  vars {
+  vars = {
    hostname = "${element(var.master_host, count.index)}.${var.domain}"
    ip = "${element(var.master_private_ip, count.index)}"
   }
@@ -13,7 +13,7 @@ data "template_file" "master_block" {
 data "template_file" "infra_block" {
   count = "${var.infra_node_count}"
   template = "$${hostname} openshift_ip=$${ip} openshift_node_group_name='node-config-infra'"
-  vars {
+  vars = {
    hostname = "${element(var.infra_host, count.index)}.${var.domain}"
    ip = "${element(var.infra_private_ip, count.index)}"
   }
@@ -22,7 +22,7 @@ data "template_file" "infra_block" {
 data "template_file" "compute_block" {
   count = "${var.app_node_count}"
   template = "$${hostname} openshift_ip=$${ip} openshift_node_group_name='node-config-compute'"
-  vars {
+  vars = {
    hostname = "${element(var.app_host, count.index)}.${var.domain}"
    ip = "${element(var.app_private_ip, count.index)}"
   }
@@ -31,7 +31,7 @@ data "template_file" "compute_block" {
 data "template_file" "gluster_block" {
   count = "${var.storage_node_count}"
   template = "$${hostname} glusterfs_ip=$${hostip} glusterfs_devices='[ \"/dev/dm-0\", \"/dev/dm-1\", \"/dev/dm-2\"]' openshift_node_group_name='node-config-compute'"
-  vars {
+  vars = {
    hostname = "${element(var.storage_host, count.index)}.${var.domain}"
    hostip = "${element(var.storage_private_ip, count.index)}"
   }
@@ -41,7 +41,7 @@ data "template_file" "gluster_block" {
 # Create the new Inventory file with master node info.
 data "template_file" "ose_inventory_new" {
   template = "${var.storage_node_count == "0" ? "${file("${path.cwd}/templates/inventorywithoutgluster.cfg.tpl")}" : "${file("${path.cwd}/templates/inventory.cfg.tpl")}"}"
-  vars {
+  vars = {
     master_hostname = "${element(var.master_host, 0)}.${var.domain}"
     infra_hostname = "${element(var.infra_host, 0)}.${var.domain}"
     app_hostname = "${element(var.app_host, 0)}.${var.domain}"
@@ -69,7 +69,7 @@ resource "local_file" "ose_inventory_file" {
 data "template_file" "master_host_file_template" {
   count = "${var.master_node_count}"
   template = "$${master_ip} $${master_hostname} $${master_hostname_domain} "
-  vars {
+  vars = {
     master_ip              = "${element(var.master_private_ip, count.index)}"
     master_hostname        = "${element(var.master_host, count.index)}"
     master_hostname_domain = "${element(var.master_host, count.index)}.${var.domain}"
@@ -79,7 +79,7 @@ data "template_file" "master_host_file_template" {
 data "template_file" "master_public_host_file_template" {
   count = "${var.master_node_count}"
   template = "$${master_ip} $${master_hostname} $${master_hostname_domain} "
-  vars {
+  vars = {
     master_ip              = "${element(var.master_public_ip, count.index)}"
     master_hostname        = "${element(var.master_host, count.index)}"
     master_hostname_domain = "${element(var.master_host, count.index)}.${var.domain}"
@@ -89,7 +89,7 @@ data "template_file" "master_public_host_file_template" {
 data "template_file" "app_host_file_template" {
   count = "${var.app_node_count}"
   template = "$${app_ip} $${app_hostname} $${app_hostname_domain} "
-  vars {
+  vars = {
     app_ip              = "${element(var.app_private_ip, count.index)}"
     app_hostname        = "${element(var.app_host, count.index)}"
     app_hostname_domain = "${element(var.app_host, count.index)}.${var.domain}"
@@ -99,7 +99,7 @@ data "template_file" "app_host_file_template" {
 data "template_file" "infra_host_file_template" {
   count = "${var.infra_node_count}"
   template = "$${infra_ip} $${infra_hostname} $${infra_hostname_domain} "
-  vars {
+  vars = {
     infra_ip              = "${element(var.infra_private_ip, count.index)}"
     infra_hostname        = "${element(var.infra_host, count.index)}"
     infra_hostname_domain = "${element(var.infra_host, count.index)}.${var.domain}"
@@ -109,7 +109,7 @@ data "template_file" "infra_host_file_template" {
 data "template_file" "storage_host_file_template" {
   count = "${var.storage_node_count}"
   template = "$${storage_ip} $${storage_hostname} $${storage_hostname_domain} "
-  vars {
+  vars = {
     storage_ip              = "${element(var.storage_private_ip, count.index)}"
     storage_hostname        = "${element(var.storage_host, count.index)}"
     storage_hostname_domain = "${element(var.storage_host, count.index)}.${var.domain}"
